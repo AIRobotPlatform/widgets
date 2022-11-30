a238bbee181bdfae644ed919ba43d63de823ee79
Roxana-Irimia <roxana@axiologic.net>
2022-11-30 17:22:52 +0200

    update preview card2

--- components/preview-card2/PreviewCardController2.js
+++ components/preview-card2/PreviewCardController2.js
@@ -3,20 +3,13 @@ const { WebcController } = WebCardinal.controllers;
 export default class PreviewCardController2 extends WebcController {
     defaultValues = {
         background: 'url(./AIRobotWidgets/components/assets/card-preview2.svg)',
-        buttonColor: '#4CBC79',
-        buttonText:'Vezi harta'
     }
     constructor(...props) {
         super(...props);
         this.model = {
             background: {
                 style: `background:${this.model.background || this.defaultValues.background}`
-            },
-            button: {
-                tag: "tap-button",
-                style: `background:${this.model.buttonColor || this.defaultValues.buttonColor}`,
-                html:this.model.buttonText||this.defaultValues.buttonText,
-            },
+            }
         };
         if(this.model.type=='large'){
             this.getElementByTag('preview-card').classList.add('type2');
@@ -27,17 +20,5 @@ export default class PreviewCardController2 extends WebcController {
         else {
             this.getElementByTag('preview-card').classList.add('type1');
         }
-        this.eventListeners();
-    }
-
-
-    eventListeners() {
-        this.onTagClick("tap-button", () => {
-            if (this.model.navigateTo === undefined) {
-                console.log("set navigateTo in parent controller");
-                return;
-            }
-            this.navigateToPageTag(this.model.navigateTo);
-        })
     }
 }
