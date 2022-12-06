41c93fd45b912ef6006299c27444a37784170de1
Roxana-Irimia <roxana@axiologic.net>
2022-12-06 12:00:30 +0200

    fix chat option positioning

--- components/chat-messages/ChatMessagesController.js
+++ components/chat-messages/ChatMessagesController.js
@@ -4,4 +4,5 @@ export default class ChatMessagesController extends WebcController {
     constructor(...props) {
         super(...props);
     }
+
 }
--- components/chat-option-menu/ChatOptionMenuController.js
+++ components/chat-option-menu/ChatOptionMenuController.js
@@ -3,12 +3,20 @@ const { WebcController } = WebCardinal.controllers;
 export default class ChatOptionMenuController extends WebcController {
     constructor(...props) {
         super(...props);
-        setTimeout(() => this.initEventListeners(), 500)
+        setTimeout(() => this.initEventListeners(), 100)
     }
     initEventListeners() {
         const options = document.querySelectorAll(".option");
+        const optionsDiv = document.querySelector(".options-container");
+        const messagesHeight = document.querySelector(".main-body").offsetHeight;
+        const height = (messagesHeight - optionsDiv.offsetHeight) + "px";
+
         options.forEach((option, index) => {
-            option.onclick = () => this.model.action(index);
+            option.onclick = () => {
+                this.model.action(index);
+                document.querySelector('.chat-page').style.setProperty('--height', messagesHeight + "px")
+            };
         })
+        document.querySelector('.chat-page').style.setProperty('--height', height)
     }
 }
