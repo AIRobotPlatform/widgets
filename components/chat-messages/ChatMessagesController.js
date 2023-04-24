f9fa6bc9e16e7a26fbf5b5dbc777f1f0523d7c58
adrian.ganga <adiganga2002@gmail.com>
2023-04-24 10:19:12 +0300

    refactor and clean-up

--- components/chat-messages/ChatMessagesController.js
+++ components/chat-messages/ChatMessagesController.js
@@ -3,7 +3,6 @@ const { WebcController } = WebCardinal.controllers;
 export default class ChatMessagesController extends WebcController {
     constructor(...props) {
         super(...props);
-        setTimeout(async () => await document.querySelector("ion-content").scrollToBottom(), 1000)
     }
 
 }
